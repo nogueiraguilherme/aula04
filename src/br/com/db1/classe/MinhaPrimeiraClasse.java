567d22e6ab4ec96bc2ba7a2bf780cb05cca4d459
Aluno <Aluno>
2018-05-11 16:46:49 -0300

    versionando arquivo clonado

--- src/br/com/db1/classe/MinhaPrimeiraClasse.java
+++ src/br/com/db1/classe/MinhaPrimeiraClasse.java
@@ -3,6 +3,6 @@ package br.com.db1.classe;
 public class MinhaPrimeiraClasse {
 	
 	public static void main(String[] args) {
-		System.out.println("oioioi");
+		System.out.println("oioioi-clonado");
 	}
 }
